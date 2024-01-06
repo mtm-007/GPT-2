5316bb0f2b56dd6145d776636f794aace43e35f1
merhawitsegay <merhawitsegay007@gmail.com>
2024-01-06 14:26:21 -0800

    self attention

--- nanogpt.py
+++ nanogpt.py
@@ -7,12 +7,13 @@ from torch.nn import functional as F
 #hyperparameters
 batch_size= 32
 block_size = 8
-max_iters = 3000
-eval_iterval = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_iterval = 500
+learning_rate = 1e-3
 device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_emb = 32
+
 #-------
 torch.manual_seed(1337)
 
@@ -61,12 +62,50 @@ def estimate_loss():
     model.train()
     return out
 
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+
+        #lets see single head perform self-attention
+        self.key = nn.Linear(n_emb, head_size, bias=False)
+        self.query = nn.Linear(n_emb, head_size, bias=False)
+        self.value = nn.Linear(n_emb, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+
+        B,T,C = x.shape
+        k = self.key(x)  # (B, T, C)
+        q = self.query(x) # (B, T, C)
+        #scaled attention is for making wei unit varinace 
+        wei = q @ k.transpose(-2, -1) * (C**0.5)# (B,T, C) @ (B, C,T) --> (B,T,T)
+        wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) # B,T,T
+        wei = F.softmax(wei, dim=-1) # B,T,T
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+class Multi_head_attention(nn.Module):
+    """ multiple heads of self attention in parallel"""
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.Modulelist([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+    
+
 class BigramLanguageModel(nn.Module):
     #def __init__(self, vocab_size):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size,n_emb)
         self.possitinal_embedding_table = nn.Embedding(block_size, n_emb)
+        self.sa_head = Multi_head_attention(4, n_emb//4) # 4 heads of 8-dim self-attention
         self.lm_head = nn.Linear(n_emb, vocab_size) 
         
 
@@ -76,7 +115,8 @@ class BigramLanguageModel(nn.Module):
         #idx, and tagets are both in dimention (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # B,T, C, Batch, T (block_size), C(n_embed C)
         posi_emb = self.possitinal_embedding_table(torch.arange(T, device = device)) # (T, C)
-        x = tok_emb + posi_emb
+        x = tok_emb + posi_emb # B,T,C
+        x = self.sa_head(x) # apply one self head attention. (B,T,C)
         logits = self.lm_head(x) # B, T, C(vocab_size C)
 
         if targets is None:
@@ -90,10 +130,14 @@ class BigramLanguageModel(nn.Module):
     
     def generate(self, idx, max_tokens):
         for _ in range(max_tokens):
-            logits,loss = self(idx)
+            #cropping the context ( idx) to the last block_size tokens, otherwise our posi_emb will run out of scope
+            idx_cond = idx[:, -block_size:]
+            #getting the predictios
+            logits,loss = self(idx_cond)
             #focus on the last of time step
             logits = logits[:,-1,:] # becomes (B, C)
             probs = F.softmax(logits, dim=-1) # (B, C)
+            # sampling from distribution
             id_next = torch.multinomial(probs, num_samples=1)# (B, C)
             idx = torch.cat((idx, id_next), dim = 1) # (B, T+1)
         return idx
