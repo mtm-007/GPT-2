1db43f38c29e162e810c444fe0010691ae86b6b1
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2025-10-25 02:21:38 +0000

    add self attention

--- train_script_adrjK/train_gpt2.py
+++ train_script_adrjK/train_gpt2.py
@@ -2,13 +2,43 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
+import math
 
 class CasualSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-
+        assert config.n_embd % config.n_head == 0
+        #key, query, value projection for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        #output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        #regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        #not really a bias more of a mask
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1,1, config.block_size, config.block_size))
+        
+    def forward(self, x):
+        B,T,C = x.size() #batch_size, sequence length, embedding dimentionality (n_embd)
+        #calculate query, key, values for all heads in batch and move head forward to be the batch
+        #nh is "number of heads", hs is "head size", and C "number of channels" = nh * hs
+        #e.g. in GPT-2(124M), n_head=12, hs=64, so nh*hs=C=768 channels in the transformers
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) #(B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) #(B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) #(B, nh, T, hs)
+        # attention (materializes the large (T, T) matrix for all the queries and keys)
+        att = (q @ k.transpose(-2,-1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T]==0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1,2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        #output projection
+        y = self.c_proj(y)
+        return y
         
 class MLP(nn.Module):
     """ 
